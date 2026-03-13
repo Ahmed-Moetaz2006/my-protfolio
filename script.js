cb7fefc35440c81dd81686429a3df9a26b9245c5
Ahmed-Moetaz2006 <ahmed.moetaz.2006@gmail.com>
2026-03-13 21:24:13 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+const btn=document.querySelector(".first");
+btn.addEventListener("click",function(){
+    document.body.style.backgroundColor="orange";
+    alert("اهلا بيك في موقع Ahmed Moetaz");
+});
