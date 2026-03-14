792b09e1a992a144377f514994ef939d40649fd4
Ahmed-Moetaz2006 <ahmed.moetaz.2006@gmail.com>
2026-03-14 20:35:02 +0200

    Delete vs.css directory

--- vs.css/java.js
+++
@@ -1,5 +0,0 @@
-const btn=document.querySelector(".first");
-btn.addEventListener("click",function(){
-    document.body.style.backgroundColor="green";
-    alart("اهلا بيك في موقع المبرمج Ahmed Moetaz");
-});
