f42e65504507c3a8d60c33e2c92f05dd6ed90cc7
Ahmed-Moetaz2006 <ahmed.moetaz.2006@gmail.com>
2026-03-18 02:08:38 +0200

    Add files via upload

---
+++ page.js
@@ -0,0 +1,12 @@
+const btn=document.querySelector(".first");
+btn.addEventListener("click",function(){
+document.body.style.backgroundcolor="white";
+alert("أهلا بيك في موقع Ahmed Moetaz");
+});
+
+        let nameText='أهلا بيك في موقع "Ahmed Moetaz"';
+          alert(nameText);
+                  document.write('<center>Names:<hr><h1>Ahmed moetaz</h1></center> <hr> </center>');
+                 
+              
+        
